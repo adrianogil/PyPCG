90af6429274dfd0082c197ea5f8a4911f1f57b26
Adriano Gil <adrianogil.san@gmail.com>
2019-01-23 13:16:00 -0400

    Added command to pcg images using cairo lines

--- src/bashrc.sh
+++ src/bashrc.sh
@@ -1,5 +1,10 @@
 if [ -z "$PYPCG_PYTHON_PATH" ]
 then
-    export PYPCG_PYTHON_PATH=$PYPCG_SOURCE_DIR/python/
+    export PYPCG_PYTHON_PATH=$PYPCG_SOURCE_DIR/
     export PYTHONPATH=$PYPCG_PYTHON_PATH:$PYTHONPATH
 fi
+
+function pcg-image-cairo-lines()
+{
+    python3 -m pcg.image.cairo_lines_001
+}
