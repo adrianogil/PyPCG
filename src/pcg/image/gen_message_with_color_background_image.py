99bd3a03f95903608b35b4327e10ed4ecad6169b
Adriano Gil <adrianogil.san@gmail.com>
2023-02-16 21:15:28 -0400

    Added script that draw a circle

--- src/pcg/image/gen_message_with_color_background_image.py
+++ src/pcg/image/gen_message_with_color_background_image.py
@@ -11,7 +11,7 @@ font = ImageFont.truetype("Chalkduster.ttf", 36)
 left, top, right, bottom = font.getbbox(text)
 
 text_width = abs(right - left)
-text_height = abs(top - left)
+text_height = abs(top - bottom)
 
 # generate a random RGB color
 r = random.randint(0, 255)
@@ -19,13 +19,19 @@ g = random.randint(0, 255)
 b = random.randint(0, 255)
 
 # create an image with the random color
-image = Image.new("RGB", (300, 300), (r, g, b))
+image_width = 300
+image_height = 300
+image = Image.new("RGB", (image_height, image_width), (r, g, b))
 
 # create an ImageDraw object
 draw = ImageDraw.Draw(image)
 
+# Center text
+text_pos_x = image_width / 2 - text_width / 2
+text_pos_y = image_height / 2 - text_height / 2
+
 # draw the text on the image
-draw.text((0, 0), text, font=font)
+draw.text((text_pos_x, text_pos_y), text, font=font)
 
 # save the image
 image.save("hello_world.png")
