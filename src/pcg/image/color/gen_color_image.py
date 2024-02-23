6c021855d073cd24ab84e9321c535ceff9da7e96
Adriano Gil <adrianogil.san@gmail.com>
2024-02-22 20:12:47 -0400

    doc: added more documentation and a number of colors as an optional argument

--- src/pcg/image/color/gen_color_image.py
+++ src/pcg/image/color/gen_color_image.py
@@ -5,18 +5,31 @@ import cv2
 import sys
 
 
+if len(sys.argv) < 4:
+    print("Usage: python gen_color_image.py <image_path> <width> <height> [total_x_colors] [total_y_colors]")
+    sys.exit(1)
+
 image_path = sys.argv[1]
 width = int(sys.argv[2])
 height = int(sys.argv[3])
 
-total_x_colors = int(sys.argv[4])
+total_x_colors = 1
 total_y_colors = 1
+
+if len(sys.argv) > 4:
+    total_x_colors = int(sys.argv[4])
 
 if len(sys.argv) > 5:
     total_y_colors = int(sys.argv[5])
 
 
 def gen_random_color():
+    """
+    Generates a random RGB color.
+
+    Returns:
+        list: A list containing three integers representing the RGB values of the color.
+    """
     return [randint(0, 255), randint(0, 255), randint(0, 255)]
 
 
